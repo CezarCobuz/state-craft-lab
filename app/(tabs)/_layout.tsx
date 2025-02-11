bed1f31dc1c17f9c864b45f2d17cc383e723e26c
CezarCobuz <cobuzcezar@gmail.com>
2025-02-11 19:22:11 +0200

    Add DynamicTabIcon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,6 +7,7 @@ import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { DynamicTabIcon } from "@/components/DynamicTabIcon";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -40,11 +41,10 @@ export default function TabLayout() {
         name="icons"
         options={{
           title: "Icons",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperclip.circle.fill" color={color} />
-          ),
+          tabBarIcon: ({ color }) => <DynamicTabIcon color={color} />,
         }}
       />
+
       <Tabs.Screen
         name="otp"
         options={{
